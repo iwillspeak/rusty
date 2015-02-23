f4fd45a0ca0c0eab32d9648c557ef7d36338c905
Will Speak <lithiumflame@gmail.com>
2015-02-23 08:16:13 +0000

    FizzBuzz

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,48 @@
-#![feature(old_io)]
-
-extern crate term;
-use std::thread;
-
 fn main() {
-    let mut t = term::stdout().unwrap();
 
-    for i in 0..1000 {
-        thread::spawn(move || {
-            println!("from thread {}", i);
-        });
+    for i in 1..100 {
+        println!(
+            "{}",
+            if div_by_fifteen(i) { "FizzBuzz".to_string() }
+            else if div_by_three(i) { "Fizz".to_string() }
+            else if div_by_five(i) { "Buzz".to_string() }
+            else { i.to_string() });
     }
 
-    t.fg(term::color::RED).unwrap();
-    writeln!(t, "this  is a test").unwrap();
+}
+
+fn div_by_three(num: u64) -> bool {
+    (num % 3) == 0
+}
+
+fn div_by_five(num: u64) -> bool {
+    (num % 5) == 0
+}
+
+fn div_by_fifteen(num: u64) -> bool {
+    div_by_five(num) && div_by_three(num)
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::div_by_three;
+
+    #[test]
+    fn test_div_by_three_returns_false() {
+
+        assert_eq!(false, div_by_three(1));
+        assert_eq!(false, div_by_three(2));
+
+        assert_eq!(false, div_by_three(4));
+        assert_eq!(false, div_by_three(5));
+    }
+
+    #[test]
+    fn test_div_by_three_returns_true() {
+        
+        for i in 1..300 {
+            assert_eq!(true, div_by_three(3 * i));
+        }
+    }
 }
