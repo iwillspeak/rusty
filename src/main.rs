785493db6bf0738cb4a64424c3237837fc99827b
Will Speak <lithiumflame@gmail.com>
2015-03-10 08:09:53 +0000

    Return Tokens from Tokensise Method
    Some initial work on lexing things...

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,41 @@
-#[cfg(not(test))]
+#![feature(collections)]
+
+use std::str::FromStr;
+
+// Token Type Enumeration
+//
+// This enum holds all of the different values we will produce when
+// tokenising a buffer
+#[derive(Debug)]
+#[allow(dead_code)]
+enum Token {
+    Plus,
+    Assign,
+    Var(String),
+    Num(usize)
+}
+
+fn tokenise(string: &str) -> Result<Vec<Token>,String> {
+    let mut tokens = vec!();
+    for part in string.split(' ') {
+        tokens.push(match part.char_at(0) {
+            '0'...'9' => {
+                let num = FromStr::from_str(part).unwrap();
+                Token::Num(num)
+            },
+            'a'...'z' => Token::Var(part.to_string()),
+            _ => {
+                return Err(format!("unrecognised token {0}", part));
+            } 
+        });
+    }
+    Ok(tokens)
+}
+
 fn main() {
-    use std::sync::mpsc;
-    use std::thread;
-
-    let (tx, rx) = mpsc::channel();
-
-    let _thr = thread::scoped(move || {
-        for i in 1u64..100u64 {
-            tx.send(Some(i)).unwrap();
-        };
-        tx.send(None).unwrap();
-    });
-
-    while let Some(i) = rx.recv().unwrap() {
-        println!(
-            "{}",
-            if div_by_fifteen(i) { "FizzBuzz".to_string() }
-            else if div_by_three(i) { "Fizz".to_string() }
-            else if div_by_five(i) { "Buzz".to_string() }
-            else { i.to_string() });
-    }
-
-    println!("Done!");
-}
-
-fn div_by_three(num: u64) -> bool {
-    (num % 3) == 0
-}
-
-fn div_by_five(num: u64) -> bool {
-    (num % 5) == 0
-}
-
-fn div_by_fifteen(num: u64) -> bool {
-    div_by_five(num) && div_by_three(num)
-}
-
-#[cfg(test)]
-mod test {
-
-    use super::div_by_three;
-    use super::div_by_five;
-    use super::div_by_fifteen;
-
-    #[test]
-    fn test_div_by_three_returns_false() {
-
-        assert_eq!(false, div_by_three(1));
-        assert_eq!(false, div_by_three(2));
-
-        assert_eq!(false, div_by_three(4));
-        assert_eq!(false, div_by_three(5));
-    }
-
-    #[test]
-    fn test_div_by_three_returns_true() {
-        
-        for i in 1..300 {
-            assert_eq!(true, div_by_three(3 * i));
-        }
-    }
-
-    #[test]
-    fn test_div_by_five_returns_false() {
-        
-        assert_eq!(false, div_by_five(3));
-        assert_eq!(false, div_by_five(4));
-        assert_eq!(false, div_by_five(1));
-        assert_eq!(false, div_by_five(11));
-        assert_eq!(false, div_by_five(19));
-    }
-
-    #[test]
-    fn test_div_by_five_returns_true() {
-        
-        for i in 1..100 {
-            assert_eq!(true, div_by_five(5 * i));
-        }
-    }
-
-    #[test]
-    fn test_div_by_fifteen_returns_false() {
-
-        assert_eq!(false, div_by_fifteen(5));
-        assert_eq!(false, div_by_fifteen(3));
-
-        assert_eq!(false, div_by_fifteen(14));
-        assert_eq!(false, div_by_fifteen(31));
-    }
-
-    #[test]
-    fn test_div_by_fifteen_returns_true() {
-        
-        for i in 1..300 {
-            assert_eq!(true, div_by_fifteen(15 * i));
-        }
-    }
+    let mut token = Token::Plus;
+    println!("{:?}", token);
+    token = Token::Var("Hello".to_string());
+    println!("{:?}", token);
+    println!("{:?}", tokenise("some 123 string"));
 }
