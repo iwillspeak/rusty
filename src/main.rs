366b838d740a4b0d891b842599d66257884bcf4a
Will Speak <lithiumflame@gmail.com>
2015-03-18 21:04:58 +0000

    Add Unit Tests
    Adds partial equality to the tokens so that we can assert on their
    values in the tests.
    
    Adds a simple test to make sure we get the identifier tokens we expect
    from a few simple strings. Also tests destructuring and comparing some
    tokens.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(collections)]
+#![feature(core)]
 
 use std::str::FromStr;
 
@@ -6,7 +7,7 @@ use std::str::FromStr;
 //
 // This enum holds all of the different values we will produce when
 // tokenising a buffer
-#[derive(Debug)]
+#[derive(Debug,PartialEq)]
 #[allow(dead_code)]
 enum Token {
     Plus,
@@ -32,10 +33,47 @@ fn tokenise(string: &str) -> Result<Vec<Token>,String> {
     Ok(tokens)
 }
 
+#[cfg(not(test))]
 fn main() {
     let mut token = Token::Plus;
     println!("{:?}", token);
     token = Token::Var("Hello".to_string());
     println!("{:?}", token);
     println!("{:?}", tokenise("some 123 string"));
+}
+
+#[test]
+fn test_tokenise_vars_returns_vars() {
+    let tokens = tokenise("hello world").unwrap();
+    assert_eq!(2, tokens.len());
+    {
+        match tokens[0] {
+            Token::Var(ref name) => assert_eq!("hello", name.as_slice()),
+            _ => panic!("Expected Token::Var")
+        }
+    }
+    {
+        match tokens[1] {
+            Token::Var(ref name) => assert_eq!("world", name.as_slice()),
+            _ => panic!("Expected Token::Var")
+        }
+    }
+}
+
+#[test]
+fn test_tokenise_numbers_returns_nums() {
+    let tokens = tokenise("1 0 1337").unwrap();
+    assert_eq!(3, tokens.len());
+    {
+        let ref token = tokens[0];
+        assert_eq!(Token::Num(1), *token);
+    }
+    {
+        let ref token = tokens[1];
+        assert_eq!(Token::Num(0), *token);
+    }
+    {
+        let ref token = tokens[2];
+        assert_eq!(Token::Num(1337), *token);
+    }
 }
