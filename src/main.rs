2da75c18bf3d654d1d2f5541900ab5c9df209687
Will Speak <lithiumflame@gmail.com>
2015-03-23 22:51:02 +0000

    Add RustLex Import
    Rustlex should enable building a simple regex-based lexer.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+#![feature(plugin)]
+#![feature(io)]
 #![feature(collections)]
-#![feature(core)]
+#![feature(box_syntax)]
+
+#![feature(core,test)]
+
+#![plugin(rustlex)]
+#[allow(plugin_as_library)]
+extern crate rustlex;
 
 use std::str::FromStr;
 
@@ -9,14 +17,22 @@ use std::str::FromStr;
 // tokenising a buffer
 #[derive(Debug,PartialEq)]
 #[allow(dead_code)]
-enum Token {
+pub enum Token {
     Plus,
     Assign,
     Var(String),
     Num(usize)
 }
 
-fn tokenise(string: &str) -> Result<Vec<Token>,String> {
+rustlex! SimpleLexer {
+    // expression definitions
+    let A = 'a';
+
+    // then rules
+    A => |lexer: &mut SimpleLexer<R>| Some(Token::Var(lexer.yystr()))
+}
+
+pub fn tokenise(string: &str) -> Result<Vec<Token>,String> {
     let mut tokens = vec!();
     for part in string.split(' ') {
         tokens.push(match part.char_at(0) {
@@ -42,38 +58,53 @@ fn main() {
     println!("{:?}", tokenise("some 123 string"));
 }
 
-#[test]
-fn test_tokenise_vars_returns_vars() {
-    let tokens = tokenise("hello world").unwrap();
-    assert_eq!(2, tokens.len());
-    {
-        match tokens[0] {
-            Token::Var(ref name) => assert_eq!("hello", name.as_slice()),
-            _ => panic!("Expected Token::Var")
-        }
-    }
-    {
-        match tokens[1] {
-            Token::Var(ref name) => assert_eq!("world", name.as_slice()),
-            _ => panic!("Expected Token::Var")
-        }
-    }
-}
 
-#[test]
-fn test_tokenise_numbers_returns_nums() {
-    let tokens = tokenise("1 0 1337").unwrap();
-    assert_eq!(3, tokens.len());
-    {
-        let ref token = tokens[0];
-        assert_eq!(Token::Num(1), *token);
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+    extern crate test;
+
+    #[test]
+    fn tokenise_vars_returns_vars() {
+        let tokens = tokenise("hello world").unwrap();
+        assert_eq!(2, tokens.len());
+        {
+            match tokens[0] {
+                Token::Var(ref name) => assert_eq!("hello", name.as_slice()),
+                _ => panic!("Expected Token::Var")
+            }
+        }
+        {
+            match tokens[1] {
+                Token::Var(ref name) => assert_eq!("world", name.as_slice()),
+                _ => panic!("Expected Token::Var")
+            }
+        }
     }
-    {
-        let ref token = tokens[1];
-        assert_eq!(Token::Num(0), *token);
+
+    #[test]
+    fn tokenise_numbers_returns_nums() {
+        let tokens = tokenise("1 0 1337").unwrap();
+        assert_eq!(3, tokens.len());
+        {
+            let ref token = tokens[0];
+            assert_eq!(Token::Num(1), *token);
+        }
+        {
+            let ref token = tokens[1];
+            assert_eq!(Token::Num(0), *token);
+        }
+        {
+            let ref token = tokens[2];
+            assert_eq!(Token::Num(1337), *token);
+        }
     }
-    {
-        let ref token = tokens[2];
-        assert_eq!(Token::Num(1337), *token);
+
+    #[bench]
+    fn bench_tokenise(b: &mut test::Bencher) {
+        b.iter(|| {
+           tokenise("identifier 1 12 1000010010010 some fooo bars")
+        });
     }
 }
